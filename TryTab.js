c862a6c3966a875698a924661b9eefaa1a2bdae4
mehroz-muzaffar-SEF18 <79619614+mehroz-muzaffar-SEF18@users.noreply.github.com>
2022-06-20 01:18:55 +0500

    Added Native Modules

--- TryTab.js
+++ TryTab.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Text,Pressable, View, Image, StyleSheet, TouchableOpacity, ScrollView } from 'react-native';
+import { Text,Pressable, View, Image, StyleSheet, TouchableOpacity, ScrollView, Button } from 'react-native';
 import shirt from './Files/Images/t-shirt.png'
 import shirtB from './Files/Images/shirtB.png'
 import jeans from './Files/Images/jeans.png'
@@ -12,6 +12,10 @@ import change from './Files/Images/change.png';
 import * as ImagePicker from 'react-native-image-picker/src';
 import { checkAllPermissions, GetAllPermissions} from './permisions';
 import * as RNFS from 'react-native-fs';
+import {NativeModules} from 'react-native';
+import { ActivityIndicator } from 'react-native';
+
+const {DressUpModule}=NativeModules;
 
 const styles = StyleSheet.create({
   bar: {
@@ -47,21 +51,35 @@ const styles = StyleSheet.create({
     alignItems:"center",
     justifyContent:"center"
   },
+  middleLayer:{
+    display: 'flex',
+    height:'75%',
+    alignItems:"center",
+    justifyContent:"center"
+  },
   pic:{
     width:"100%",
     height:"100%",
+  },
+  pic_border:{
     borderWidth:2,
     borderColor:"grey"
   },
+  pic_selected_border:{
+    borderWidth:4,
+    borderColor:"skyblue"
+  },
   logo: {
+    backgroundColor:'black',
     width: "90%",
     height: "90%",
-    borderWidth:2,
-    borderRadius:10,
-    borderColor: "grey"
+    // borderWidth:2,
+    // borderRadius:10,
+    // borderColor: "grey"
   },
   scroll:{
-    marginBottom:5
+    height:"12%",
+    paddingBottom:5
   },
   button: {
     backgroundColor: "#3195CD",
@@ -73,6 +91,12 @@ const styles = StyleSheet.create({
     marginTop:20,
     marginBottom:20,
     borderRadius: 100,
+  },
+  buttonBlock: {
+    backgroundColor: "green",
+    flex:1,
+    justifyContent:"center",
+    alignItems:"center"
   },
   buttonClose: {
     backgroundColor: "#3195CD",
@@ -103,8 +127,19 @@ const TryTab = (props) => {
   var [pantTryName, setPantTryName] = React.useState(props.pant);//pant name for the pant that will be tried on user's pic. use it in image source by using this {uri:"file://"+RNFS.ExternalDirectoryPath+'/'+pantpath} in image source
   var [shoeTryName, setShoeTryName] = React.useState(props.shoe);//shoe name for the shoe that will be tried on user's pic. use it in image source by using this {uri:"file://"+RNFS.ExternalDirectoryPath+'/'+shoepath} in image source
   var [firstTime,setFirstTime]=React.useState(true);//Hmmm... First Time?
+  var [counter,setCounter]=React.useState(0);//For Testing Purposes
+  var [prevName,setPrevName]=React.useState(null);
+  var [personPicPath,setPersonPicPath]=React.useState(null);
+  var [isLoading, setIsLoading]=React.useState(false);
   // States End here-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+  React.useEffect(()=>{
+    if(isLoading)
+    {
+      TryOnSelectedClothes(shirtTryName, pantTryName, shoeTryName);
+    }
+  },[isLoading])
   if(firstTime){
+    cleanPreviousPictures();
     readFilesForScroll(1);// why is it called? its because when try tab is opened by default shirts part/fragment is opened hence 1 is added in category so that it loads all shirt names
     setFirstTime(false);
   }
@@ -132,6 +167,7 @@ const TryTab = (props) => {
         }
         else {
         setFilepath(res.assets[0].uri);
+        setPersonPicPath(res.assets[0].uri);
        }
         });
   }
@@ -165,10 +201,18 @@ const TryTab = (props) => {
   function middleLayer() // dont change it, it updates the middle layer
   {
     var items=[]
+    if(isLoading)
+    {
+      items.push(
+        <View key={3} style={[styles.middleLayer]}>
+          <ActivityIndicator size={50} style={[styles.logo]} />
+        </View>);
+      return items;
+    }
     if (!istapped)
     {
       items.push(
-      <View key={1} style={[styles.container1,styles.container2]}>
+      <View key={1} style={[styles.middleLayer]}>
         <TouchableOpacity style={styles.button} onPress={()=>{GalleryIntent();setIstapped(true);}}>
           <View style={{justifyContent:"center", flexDirection:'row'}}>
           <Image style={{marginRight:22, width:20, height:20}} source={gallery} ></Image>
@@ -183,9 +227,9 @@ const TryTab = (props) => {
     else
     {
       items.push(
-      <View key={2} style={[styles.container1,styles.container2]}>
-        <Image style={styles.logo} source={filepath==null?sample:{uri:filepath}} ></Image>
-        <TouchableOpacity style={[styles.buttonClose]} onPress={()=>{setIstapped(false);setFilepath(null);}}>
+      <View key={2} style={[styles.middleLayer]}>
+        <Image style={styles.logo} resizeMode='contain' source={filepath==null?sample:{uri:filepath}} ></Image>
+        <TouchableOpacity style={[styles.buttonClose]} onPress={()=>{setIstapped(false);setFilepath(null);setPersonPicPath(null);}}>
           <Image style={{width:30,height:30}} source={change}/>
         </TouchableOpacity>
       </View>
@@ -194,7 +238,42 @@ const TryTab = (props) => {
   return items;
 }
 
-function correctPressFunction(category, name)//function that saves the chosen object name to the appropriate variable to try on user pic
+/*******BSEF18M004-MEHROZ MUZAFFAR*********/
+
+function generateFileName(prefix)
+{
+  let tempCurDate=new Date();
+  let currentDate=JSON.stringify(tempCurDate).split('\"')[1].split('T')[0];
+  let currentTime=tempCurDate.getHours()+"-"+tempCurDate.getMinutes()+"-"+tempCurDate.getSeconds();
+  let id=currentDate+"_"+currentTime;
+  let generatedFileName=prefix+"_"+id+".png";
+  return generatedFileName;
+}
+async function deletePicture(fileName)
+{
+  let imagePath=RNFS.ExternalDirectoryPath+'/'+fileName;
+  let exists = await RNFS.exists(imagePath);
+  if(exists){
+      // exists call delete
+      await RNFS.unlink(imagePath);
+  }else{
+      console.log("File Not Available")
+  }
+}
+async function cleanPreviousPictures()
+{
+  const imageFiles = await RNFS.readDir(RNFS.ExternalDirectoryPath);
+  for(let i=0;i<imageFiles.length;i++){
+    let imageName=imageFiles[i].name;
+    if(imageName.includes("gs")){
+      deletePicture(imageName);
+    }
+  }
+}
+
+/******************************************/
+
+async function correctPressFunction(category, name)//function that saves the chosen object name to the appropriate variable to try on user pic
 {
   if (category==1)
   {
@@ -210,12 +289,46 @@ function correctPressFunction(category, name)//function that saves the chosen ob
   }
 }
 
+async function TryOnSelectedClothes(shirt, pant, shoe)
+{
+  shirt=(shirt==null?"":shirt);
+  pant=(pant==null?"":pant);
+  shoe=(shoe==null?"":shoe);
+  personPicPath=(personPicPath==null?"":personPicPath);
+  if(shirt=="" && pant=="" && shoe=="")
+  {
+    alert("Please select atleast one clothe image!");
+    setIsLoading(false);
+  }
+  else if(personPicPath=="")
+  {
+    alert("Please add personal picture!");
+    setIsLoading(false);
+  }
+  else
+  {
+    // // console.log("Before: "+filepath);
+    let resultFileName=generateFileName("gs");
+    DressUpModule.TryOnClothes(personPicPath.replace("file://",""), shirt, pant, shoe, resultFileName, async (dpath,oldFileName)=>{
+      if(prevName!=null){
+        deletePicture(prevName);
+      }
+      setPrevName(oldFileName);
+      setFilepath("file://"+dpath);
+      setIsLoading(false);
+      // console.log("After: "+dpath);
+    });
+  }
+}
+
 function bottomLayerExtended(fileN) // dont change it, it updates the horizontal scrollbar on the bottom
 {
-  var comp=<Pressable key={fileN} style={({ pressed }) => [{ height: pressed ? 100 : 70, width: pressed ? 100 : 90, paddingLeft:10,alignSelf:'flex-end', paddingRight:10}]} 
-  onPress={()=>{correctPressFunction(fileN.includes("shirt")?1:(fileN.includes("pant")?2:3),fileN)}}>
-    <Image style={styles.pic} source={ fileN!=""?{uri:"file://"+RNFS.ExternalDirectoryPath+'/'+fileN}:sample} />
-  </Pressable>;
+  var comp=<TouchableOpacity key={fileN} style={{paddingLeft: 5, paddingRight: 5,height:"100%",width:75, display: 'flex', justifyContent:'center', alignItems:'center'}}>
+    <Pressable style={({ pressed }) => [{ height: pressed ? "90%" : "100%", width: pressed ? "90%" : "100%"}]} 
+    onPress={()=>{correctPressFunction(fileN.includes("shirt")?1:(fileN.includes("pant")?2:3),fileN)}}>
+      <Image style={[styles.pic,((fileN==shirtTryName || fileN==pantTryName || fileN==shoeTryName)?styles.pic_selected_border:styles.pic_border)]} source={ fileN!=""?{uri:"file://"+RNFS.ExternalDirectoryPath+'/'+fileN}:sample} />
+    </Pressable>
+  </TouchableOpacity>
   return comp;
 }
   
@@ -237,11 +350,16 @@ return(
       </TouchableOpacity>
     </View>
     {middleLayer()}
-    <View style={[styles.scroll,{position:'absolute', bottom:0}]}>
+    <View style={styles.scroll}>
       <ScrollView horizontal={true} >
-      {BottomLayer()}  
+      {BottomLayer()}
       </ScrollView>
     </View>
+    <TouchableOpacity style={styles.buttonBlock} onPress={()=>{setIsLoading(true);}}>
+        <Text style={{color:"white"}}>
+          TRY ON
+        </Text>
+    </TouchableOpacity>
   </View>
   );
 }
